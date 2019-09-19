d7a83d73eb15d5b67375f4c99305f8864479fb6e
takumanakamura0820 <hgcttk820@yahoo.co.jp>
2019-09-19 14:44:28 +0900

    hogehoge

--- app/Controller/story/edit.php
+++ app/Controller/story/edit.php
@@ -16,6 +16,17 @@ $app->get('/story/{story_id}/{page_id}/edit', function (Request $request, Respon
 
 });
 
+// $app->get('/story', function (Request $request, Response $response) {
+//
+//     //GETされた内容を取得します。
+//     $data = $request->getQueryParams();
+//
+//     // Render index view
+//     return $this->view->render($response, 'story/edit.twig', $data);
+//
+// });
+
+
 
 // 会員登録処理コントローラ
 $app->post('/story/{story_id}/{page_id}/edit', function (Request $request, Response $response) {
