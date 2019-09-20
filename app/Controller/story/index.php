93f53198c3c48a0ad9a2eb92a1c46c5325bb5bc0
kochi <c011610813@edu.teu.ac.jp>
2019-09-20 13:34:42 +0900

    ストーリー一覧のコントローラ作成

--- app/Controller/story/index.php
+++ app/Controller/story/index.php
@@ -3,6 +3,7 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Model\Dao\User;
+use Model\Dao\Story;
 
 
 // ストーリー一覧ページコントローラ
@@ -11,6 +12,24 @@ $app->get('/story/', function (Request $request, Response $response) {
     //GETされた内容を取得します。
     $data = $request->getQueryParams();
 
+	//DB通信用インスタンス作成
+    $story = new Story($this->db);
+	$user=new User($this->db);
+
+	//ストーリー情報全権取得
+	$stories=$story->select(array(),"","",null,true);
+
+	$data=array();
+	foreach ($stories as $story){
+		unset($story["created_at"]);
+		unset($story["updated_at"]);
+		unset($story["next_id"]);
+		$story["author"]=$user->select(
+			array("id"=>$story["user_id"]),"","",1,false
+			)["name"];
+		unset($story["user_id"]);
+		$data[]=$story;
+	}
     // Render index view
     return $this->view->render($response, 'story/index.twig', $data);
 });
