04452d06d28ba0b385a93392d5293823a40ca8ae
nemoto2 <nemoto2@example.com>
2019-09-20 14:11:51 +0900

    treeにデータ

--- app/Model/Dao/Selection.php
+++ app/Model/Dao/Selection.php
@@ -12,6 +12,45 @@ namespace Model\Dao;
  */
 class Selection extends Dao
 {
+    public function getSelectionByStoryId($storyId)
+    {
 
+        $sql = "
+            select 
+                selection.page_id
+                ,ahead
+                ,selection.content
+                from 
+                selection 
+                left join page on page.page_id = selection.page_id
+                where selection.story_id = :story_id
+                group by selection.page_id , selection.ahead , selection.content
+        ";
+
+        $statement = $this->db->prepare($sql);
+        $statement->bindParam("story_id", $storyId);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+
+    public function getSelectionByStoryIdAndPageId($storyId, $pageId)
+    {
+
+        $sql = "
+            select 
+                selection.page_id
+                ,ahead
+                ,selection.content
+                from 
+                selection 
+                where selection.story_id = :story_id and selection.page_id = :page_id
+        ";
+
+        $statement = $this->db->prepare($sql);
+        $statement->bindParam("story_id", $storyId);
+        $statement->bindParam("page_id", $pageId);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
 
 }
